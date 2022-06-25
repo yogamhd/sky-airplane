83dfc49e6044b10f27694d9f330c2a7cd8175a3f
yogamhd <yogapratamamhd@gmail.com>
2022-06-25 23:58:21 +0700

    edit animate

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const airplane = document.getElementById('airplane');
 function animate() {
     airplane
     .velocity({
-        top: '100px',
+        top: '0px',
+        left: '40%',
+        transform: ["rotate(10deg)","rotate(0deg)"]
+    },{
+        duration: 7000,
+    })
+
+    .velocity({
+        top: '0px',
+        left: '30%',
+        transform: ["rotate(0deg)","rotate(10deg)"]
     },{
         duration: 7000,
     })
