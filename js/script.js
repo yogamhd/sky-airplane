f17d8200433d5c62a445a08fbd892914c82bb66a
yogamhd <yogapratamamhd@gmail.com>
2022-06-26 09:47:55 +0700

    edit animate, add const, sedit style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const airplane = document.getElementById('airplane');
+const manJump = document.getElementById('man-jump');
 
 function animate() {
     airplane
@@ -11,12 +12,37 @@ function animate() {
     })
 
     .velocity({
-        top: '0px',
+        // top: '0px',
         left: '30%',
         transform: ["rotate(0deg)","rotate(10deg)"]
     },{
         duration: 7000,
     })
+
+    .velocity({
+        top: '100px',
+        left: '-500px',
+        transform: ["rotate(-5deg)","rotate(0deg)"]
+    },{
+        duration: 15000,
+    })
+
+    manJump
+        .velocity({
+            opacity: 1,
+        }, {
+            delay: 13000,
+            duration: 1000
+        })
+
+        .velocity({
+            top: window.innerHeight + 100,
+        }, {
+            queue: false,
+            delay: 13000,
+            duration: 10000
+        })
+
 }
 
 animate()
